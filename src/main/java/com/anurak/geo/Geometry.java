94dc881c937f3744e438fafa829efcd926805c11
Anurak Yutthanawa <anu.01rob@outlook.com>
2021-07-14 09:48:23 +0700

    fix problem and decrease complex in superclass

--- src/main/java/com/anurak/geo/Geometry.java
+++ src/main/java/com/anurak/geo/Geometry.java
@@ -5,8 +5,6 @@
  */
 package com.anurak.geo;
 
-import java.lang.Math;
-
 /**
  *
  * @author anurak
@@ -23,129 +21,7 @@ abstract class Geometry{
     }
 }
 
-class Circle extends Geometry{
-    private double radius;
-    private final double pi = 22.0/7;
-    private double area;
-    
-    @Override
-    public void getValue(){
-        System.out.println("Radius: "+radius);
-    }
-    
-    @Override
-    public void setValue(double radius){
-        if (radius <= 0){
-            System.out.println("Radius must greater than 0");
-            return;
-        }
-        this.radius = radius;
-        area = (this.pi * Math.pow((this.radius),2));
-    }
-    
-//    public void setRadius(double radius){
-//        if (radius <= 0){
-//            System.out.println("Radius must greater than 0");
-//            return;
-//        }
-//        this.radius = radius;
-//        setArea(Math.pow(this.radius, pi));
-//    }
-    @Override
-    public String toString(){
-        return String.format("This circle area is: %.2f", area);
-    }
-}
 
-class Square extends Geometry{
-    private double length;
-    private double area;
-    
-    @Override
-    public void getValue(){
-        System.out.println("Length: "+length);
-    }
-    
-//    void getLength(){
-//        System.out.println("Length: "+length);
-//    }
-    @Override
-    public void setValue(double len){
-        if (len <= 0){
-            System.out.println("Length must greater than 0");
-            return;
-        }
-        this.length = len;
-        area = (this.length * this.length);
-    }
 
-    @Override
-    public String toString(){
-        return String.format("This square area is: %.2f", area);
-    }
-    
-//    public void setLength(double len){
-//        if (len <= 0){
-//            System.out.println("Length must greater than 0");
-//            return;
-//        }
-//        this.length = len;
-//        setArea(this.length * this.length);
-//    }   
-}
 
-class Triangle extends Geometry{
-    private double width;
-    private double height;
-    private double area;
-    private final double fml = 0.5;
-    
-    @Override
-    public void setValue(double width, double height){
-        if (width <= 0){
-            System.out.println("Width must greater than 0");
-            return;
-        }
-        if (height <= 0){
-            System.out.println("Height must greater than 0");
-            return;
-        }
-        this.width = width;
-        this.height = height;
-        area = fml * height * width;
-    }
-    
-//    void setWidth(double width){
-//        if (width <= 0){
-//            System.out.println("Width must greater than 0");
-//            return;
-//        }
-//        this.width = width;
-//    }   
-//    
-//    void setHeight(double height){
-//        if (height <= 0){
-//            System.out.println("Height must greater than 0");
-//            return;
-//        }
-//        this.height = height;
-//    }
-    
-    @Override
-    public void getValue(){
-        System.out.println("Width: "+width);
-        System.out.println("Height: "+height);
-    }
 
-    @Override
-    public String toString(){
-        return String.format("This triangle area is: %.2f", area);
-    }
-    
-//    void getWidth(){
-//        System.out.println("Width: "+width);
-//    }
-//    void getHeight(){
-//        System.out.println("Height: "+height);
-//    }
-}
--- src/main/java/com/anurak/geo/Main.java
+++ src/main/java/com/anurak/geo/Main.java
@@ -11,7 +11,6 @@ package com.anurak.geo;
  */
 public class Main {
     public static void main(String[] args) {
-        
         Geometry geo1 = new Circle();
         geo1.setValue(2);
         System.out.println(geo1);
